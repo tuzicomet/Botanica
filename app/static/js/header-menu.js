80ca29c6fd35630e362a8d1f49eaf811333f3e4e
tuzicomet <leo336570@gmail.com>
2023-12-30 20:50:24 +1300

    When opening menu bar, rest of screen now dims
    Done by using an overlay which appears under the menu bar but under the rest of the screen, and only appears when the menu bar is visible.

--- app/static/js/header-menu.js
+++ app/static/js/header-menu.js
@@ -1,11 +1,13 @@
-// Wait for the DOM content to be fully loaded
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the menu icon and menu bar elements
-    const menuIcon = document.querySelector('.menu-icon');
-    const menuBar = document.querySelector('.menu-bar');
+// Get the menu icon, menu bar, and overlay elements
+const menuIcon = document.querySelector('.menu-icon');
+const menuBar = document.querySelector('.menu-bar');
+const overlay = document.querySelector('.overlay');
 
-    // Toggle the 'active' class on the menu bar when the icon is clicked
-    menuIcon.addEventListener('click', function () {
-        menuBar.classList.toggle('active');
-    });
+// Toggle the 'active' class on the menu bar when the icon is clicked
+menuIcon.addEventListener('click', () => {
+    // Toggle the 'active' class on the menu bar
+    menuBar.classList.toggle('active');
+
+    // Toggle the 'open' class on the overlay for dimming effect
+    overlay.classList.toggle('open');
 });
