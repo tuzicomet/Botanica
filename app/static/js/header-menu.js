abddc901ed8c00c5518eb0284c745a6504b8de32
tuzicomet <leo336570@gmail.com>
2023-12-30 21:12:48 +1300

    Added more header-menu.js comments
    Added a few more comments for more clarity and explanation for what's going on

--- app/static/js/header-menu.js
+++ app/static/js/header-menu.js
@@ -14,16 +14,19 @@ const overlay = document.querySelector('.overlay');
 
 // Toggle the 'active' class on the menu bar when the icon is clicked
 menuIcon.addEventListener('click', () => {
-    // Toggle the 'active' class on the menu bar
+    // Toggle the 'active' class on the menu bar for opening and closing the menu
     menuBar.classList.toggle('active');
-    // Toggle the 'open' class on the overlay for dimming effect
+    // Toggle the 'open' class on the overlay for a dimming effect when the menu is open
     overlay.classList.toggle('open');
 });
 
 // Add an event listener to close the menu when clicking outside the menu bar
 document.addEventListener('click', (event) => {
+    // Check if the click is outside the menu bar and menu icon
     if (!menuBar.contains(event.target) && !menuIcon.contains(event.target)) {
+        // Remove the 'active' class from the menu bar to close the menu
         menuBar.classList.remove('active');
+        // Remove the 'open' class from the overlay to remove the dimming effect
         overlay.classList.remove('open');
     }
 });
