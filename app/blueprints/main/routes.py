b3cce71503d7096b39b25514779c4d339f4e34ef
tuzicomet <leo336570@gmail.com>
2024-01-10 15:56:49 +1300

    Comments in routes.py and views.py updated
    comments updated to give better information about what things do

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -14,27 +14,47 @@ from .views import *
 # Define routes for the 'main' blueprint
 # (define routes for pages using this blueprint)
 
-# NOTE: enpoint parameter specifies a unique name that can be used when generating URLs
-# eg url_for('main.home')
-
-# Define a route for the home page
 @main_bp.route('/', endpoint='home')
 def home():
+    """
+    Define a route for the home page.
+
+    NOTE: endpoint='home' parameter specifies a unique name that can be used when 
+    generating URLs. eg url_for('main.home')
+
+    Returns:
+        rendered_template: Rendered HTML template for the home page.
+    """
     return render_home_page()
 
-# Define a route for the login page
 @main_bp.route('/login')
 def login():
+    """
+    Define a route for the login page.
+
+    Returns:
+        rendered_template: Rendered HTML template for the login page.
+    """
     return render_login_page()
 
-# Define a route for the forum page
 @main_bp.route('/forum')
 def forum():
+    """
+    Define a route for the forum page.
+
+    Returns:
+        rendered_template: Rendered HTML template for the forum page.
+    """
     return render_forum_page()
 
-# Define a route for the marketplace page
 @main_bp.route('/marketplace')
 def marketplace():
+    """
+    Define a route for the marketplace page.
+
+    Returns:
+        rendered_template: Rendered HTML template for the marketplace page.
+    """
     return render_marketplace_page()
 
 # Add more routes as needed
--- app/blueprints/main/views.py
+++ app/blueprints/main/views.py
@@ -1,7 +1,7 @@
 """
     blueprints/main/views.py
 
-    This file contains the route definitions for the main blueprint.
+    This file contains view functions for rendering HTML templates.
     
     Author: Leonard Lau
     Last Updated: 10/01/2023
@@ -12,19 +12,39 @@ from flask import render_template
 # Define view functions for the 'main' blueprint
 
 def render_home_page():
-    # Render the home page
+    """
+    Render the home page.
+
+    Returns:
+        rendered_template: Rendered HTML template for the home page.
+    """
     return render_template('pages/index.html')
 
 def render_login_page():
-    # Render the login page
+    """
+    Render the login page.
+
+    Returns:
+        rendered_template: Rendered HTML template for the login page.
+    """
     return render_template('pages/login.html')
 
 def render_forum_page():
-    # Render the forum page
+    """
+    Render the forum page.
+
+    Returns:
+        rendered_template: Rendered HTML template for the forum page.
+    """
     return render_template('pages/forum.html')
 
 def render_marketplace_page():
-    # Render the marketplace page
+    """
+    Render the marketplace page.
+
+    Returns:
+        rendered_template: Rendered HTML template for the marketplace page.
+    """
     return render_template('pages/marketplace.html')
 
 # Add more view functions as needed
